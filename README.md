f815641808d467f77554db2d4e1096e54000379b
S Anand <root.node@gmail.com>
2024-06-23 16:08:53 +0530

    ENH: Add OpenAI API Key

--- README.md
+++ README.md
@@ -2,7 +2,27 @@
 
 This is an authorizing proxy for LLMs.
 
+## Usage
+
+Instead of sending an API request to `https://api.openai.com/`:
+
+- Replace `https://api.openai.com/` with `https://aiproxy.sanand.workers.dev/openai/`
+
+For example:
+
+```shell
+curl -X POST http://aiproxy.sanand.workers.dev/openai/v1/chat/completions \
+  -H "Content-Type: application/json" \
+  -d '{"model": "gpt-3.5-turbo", "messages": [{"role": "user", "content": "What is 2 + 2"}]}'
+```
+
 ## Setup
 
 - Log into <https://dash.cloudflare.com/> as <root.node@gmail.com>
 - Create a worker called `aiproxy` deployed at <https://aiproxy.sanand.workers.dev>
+- Clone [this repository](https://github.com/sanand0/aiproxy)
+- Run `npm install` to install dependencies
+- Run `npm run lint` to validate code
+- Run `wrangler secret put <key>` also add them to `.dev.vars` as `KEY=value`:
+  - `OPENAI_API_KEY`
+- Run `npm run deploy` to deploy on Cloudflare
--- worker.js
+++ worker.js
@@ -1,15 +1,21 @@
 export default {
-  async fetch(request) {
-    const { method, headers } = request;
+  async fetch(request, env) {
+    // We use plugins to handle different LLMs.
+    // The plugin is the first part of the path between /.../ -- e.g. /openai/
     const url = new URL(request.url);
-    const targetUrl = `https://api.openai.com${url.pathname}${url.search}`;
+    const plugin = url.pathname.split("/")[1];
+
+    // Let the user know if there's no plugin or an unknown plugin
+    if (!plugin) return jsonResponse({ code: 200, message: "See docs at https://github.com/sanand0/aiproxy" });
+    if (!plugins[plugin]) return jsonResponse({ code: 404, error: `Unknown plugin: ${plugin}` });
+
+    const { method, url: targetUrl, headers, body } = await plugins[plugin].request({ url, request, env });
 
     const proxyRequest = new Request(targetUrl, {
       method,
       headers: skipHeaders(headers, skipRequestHeaders),
-      body: request.body,
+      body,
     });
-
     const response = await fetch(proxyRequest);
     const { readable, writable } = new TransformStream();
     response.body.pipeTo(writable);
@@ -22,6 +28,23 @@ export default {
   },
 };
 
+const plugins = {
+  openai: {
+    request: async function ({ url, request, env: { OPENAI_API_KEY } }) {
+      // Strip the plugin part of the URL to get the target URL
+      const targetPath = url.pathname.replace(/^\/openai\//, "/");
+      const headers = skipHeaders(request.headers, []);
+      headers.append("Authorization", `Bearer ${OPENAI_API_KEY}`);
+      return {
+        url: `https://api.openai.com${targetPath}${url.search}`,
+        method: request.method,
+        headers,
+        body: request.body,
+      };
+    },
+  },
+};
+
 const skipRequestHeaders = [/^content-length$/i, /^host$/i, /^cf-.*$/i, /^connection$/i, /^accept-encoding$/i];
 const skipResponseHeaders = [/^content-length$/i, /^transfer-encoding$/i, /^connection$/i];
 
@@ -29,4 +52,11 @@ function skipHeaders(headers, skipList) {
   const result = new Headers();
   for (const [key, value] of headers) if (!skipList.some((pattern) => pattern.test(key))) result.append(key, value);
   return result;
+}
+
+function jsonResponse({ code, ...rest }) {
+  return new Response(JSON.stringify(rest), {
+    status: code,
+    headers: { "content-type": "application/json" },
+  });
 }
