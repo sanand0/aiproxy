0f09b8e60e4e9e60595feb0b44db60014b97adee
S Anand <root.node@gmail.com>
2024-07-04 07:44:42 +0530

    ENH: Add validations and cost calculation

--- README.md
+++ README.md
@@ -4,19 +4,47 @@ This is an authorizing proxy for LLMs.
 
 ## Usage
 
-Instead of sending an API request to `https://api.openai.com/`:
+Get an API token from the IITM Data Science support team.
+
+Then, instead of sending an API request to `https://api.openai.com/`:
 
 - Replace `https://api.openai.com/` with `https://aiproxy.sanand.workers.dev/openai/`
+- Replace the OPENAI_API_KEY with the AIPROXY_TOKEN
 
 For example:
 
 ```shell
 curl -X POST http://aiproxy.sanand.workers.dev/openai/v1/chat/completions \
   -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $AIPROXY_TOKEN" \
   -d '{"model": "gpt-3.5-turbo", "messages": [{"role": "user", "content": "What is 2 + 2"}]}'
+
+curl -X POST http://aiproxy.sanand.workers.dev/openai/v1/embeddings \
+  -H "Content-Type: application/json" \
+  -H "Authorization: Bearer $AIPROXY_TOKEN" \
+  -d '{"model": "text-embedding-3-small", "input": ["king", "queen"]}'
 ```
 
+AI Proxy only supports these endpoints and models:
+
+- `GET https://aiproxy.sanand.workers.dev/v1/models`
+- `POST https://aiproxy.sanand.workers.dev/v1/embeddings`
+  - `model: text-embedding-3-small`
+- `POST https://aiproxy.sanand.workers.dev/v1/chat/completions`
+  - `model: gpt-3.5-turbo`
+
+It returns a set of validation headers. #TODO
+
 ## Setup
+
+MongoDB setup
+
+- Log into <https://cloud.mongodb.com/> as <anand@study.iitm.ac.in>
+- Under Network Access, add [CloudFlare IP ranges](https://www.cloudflare.com/en-in/ips/)
+- Create a cluster called `iitm-tds`
+- Enable Data API
+
+App setup
 
 - Log into <https://dash.cloudflare.com/> as <root.node@gmail.com>
 - Create a worker called `aiproxy` deployed at <https://aiproxy.sanand.workers.dev>
@@ -24,5 +52,15 @@ curl -X POST http://aiproxy.sanand.workers.dev/openai/v1/chat/completions \
 - Run `npm install` to install dependencies
 - Run `npm run lint` to validate code
 - Run `wrangler secret put <key>` also add them to `.dev.vars` as `KEY=value`:
-  - `OPENAI_API_KEY`
+  - `AIPROXY_TOKEN_SECRET`: Generated via `crypto.randomBytes(32).toString('base64url')`
+  - `OPENAI_API_KEY`: Via [OpenAI API Keys](https://platform.openai.com/api-keys)
+  - `MONGODB_APP_ID`: Via [MongoDB](https://cloud.mongodb.com/) > Data API > App Services > (App) > App ID
+  - `MONGODB_API_KEY`: Via [MongoDB](https://cloud.mongodb.com/) > Data API > Users > Create API Key
 - Run `npm run deploy` to deploy on Cloudflare
+
+To create an API token for an `emailId`, run:
+
+```js
+secret = new TextEncoder().encode(AIPROXY_TOKEN_SECRET);
+token = await new jose.SignJWT({ email: emailId }).setProtectedHeader({ alg: "HS256" }).sign(secret);
+```
--- worker.js
+++ worker.js
@@ -1,3 +1,5 @@
+import * as jose from "jose";
+
 export default {
   async fetch(request, env) {
     // We use plugins to handle different LLMs.
@@ -7,20 +9,58 @@ export default {
 
     // Let the user know if there's no plugin or an unknown plugin
     if (!plugin) return jsonResponse({ code: 200, message: "See docs at https://github.com/sanand0/aiproxy" });
-    if (!plugins[plugin]) return jsonResponse({ code: 404, error: `Unknown plugin: ${plugin}` });
+    if (!plugins[plugin]) return jsonResponse({ code: 404, message: `Unknown plugin: ${plugin}` });
 
-    const { method, url: targetUrl, headers, body } = await plugins[plugin].request({ url, request, env });
+    // Get the Authorization: Bearer token, stripping the "Bearer " and whitespace
+    const token = (request.headers.get("Authorization") ?? "").replace(/^Bearer\s+/, "").trim();
+    let payload;
+    if (!token)
+      return jsonResponse({
+        code: 401,
+        message: "Missing Authorization: Bearer header. See https://github.com/sanand0/aiproxy",
+      });
+    const secret = new TextEncoder().encode(env.AIPROXY_TOKEN_SECRET);
+    try {
+      payload = (await jose.jwtVerify(token, secret)).payload;
+      if (!payload.email)
+        return jsonResponse({ code: 401, message: `Bearer ${token} is invalid: email not found in payload` });
+    } catch (err) {
+      return jsonResponse({ code: 401, message: `Bearer ${token} is invalid: ${err}` });
+    }
 
-    const proxyRequest = new Request(targetUrl, {
+    // Check if user has quota left
+    const today = new Date().toISOString().split("T")[0];
+    const usage = await mongoRequest("findOne", { filter: { user: payload.email, date: today } }, env);
+    if (usage.error) return jsonResponse({ code: 500, message: `MongoDB error: ${usage.error}` });
+    const dailyCost = usage?.document?.dailyCost;
+    const limit = 1.0;
+    if (dailyCost > limit)
+      return jsonResponse({ code: 429, message: `On ${today} you used $${dailyCost}, exceeding $${limit}` });
+
+    let body;
+    try {
+      body = await plugins[plugin].validate(request);
+    } catch (err) {
+      return jsonResponse({ code: err.code, message: err.message });
+    }
+
+    const { method, url: targetUrl, headers } = await plugins[plugin].request({ url, request, env });
+
+    const response = await fetch(targetUrl, {
       method,
       headers: skipHeaders(headers, skipRequestHeaders),
-      body,
+      body: JSON.stringify(body),
     });
-    const response = await fetch(proxyRequest);
-    const { readable, writable } = new TransformStream();
-    response.body.pipeTo(writable);
+    const result = await response.json();
 
-    return new Response(readable, {
+    result.dailyCost = plugins[plugin].cost(result) + (usage.document?.dailyCost ?? 0);
+    await mongoRequest(
+      "updateOne",
+      { filter: { user: payload.email, date: today }, update: { $set: { dailyCost: result.dailyCost } } },
+      env,
+    );
+
+    return new Response(JSON.stringify(result, null, 2), {
       headers: skipHeaders(response.headers, skipResponseHeaders),
       status: response.status,
       statusText: response.statusText,
@@ -34,13 +74,42 @@ const plugins = {
       // Strip the plugin part of the URL to get the target URL
       const targetPath = url.pathname.replace(/^\/openai\//, "/");
       const headers = skipHeaders(request.headers, []);
-      headers.append("Authorization", `Bearer ${OPENAI_API_KEY}`);
+      headers.set("Authorization", `Bearer ${OPENAI_API_KEY}`);
       return {
         url: `https://api.openai.com${targetPath}${url.search}`,
         method: request.method,
         headers,
-        body: request.body,
       };
+    },
+    validate: async function (request) {
+      const url = new URL(request.url);
+      // Ensure that the body is valid JSON
+      let body;
+      try {
+        body = request.method == "POST" ? await request.json() : undefined;
+      } catch (err) {
+        throw new CustomError({ code: 400, message: `Invalid JSON body: ${err}` });
+      }
+      // Ensure that the model is valid
+      if (body && body.model && !["text-embedding-3-small", "gpt-3.5-turbo"].includes(body.model))
+        throw new CustomError({ code: 400, message: `Invalid model: ${body.model}` });
+      // Allow only requests to /chat/completions, /embeddings, /models
+      if (
+        url.pathname != "/openai/v1/chat/completions" &&
+        url.pathname != "/openai/v1/embeddings" &&
+        url.pathname != "/openai/v1/models"
+      )
+        throw new CustomError({ code: 400, message: `Invalid path: ${url.pathname}` });
+      // Ensure that the streaming is disabled
+      if (body && body.stream) throw new CustomError({ code: 400, message: `Streaming is not supported` });
+      return body;
+    },
+    cost: function (result) {
+      return result.model == "text-embedding-3-small"
+        ? (0.02 / 1e6) * result.usage?.prompt_tokens
+        : result.model.match(/gpt-3.5-turbo/)
+          ? (3 / 1e6) * result.usage?.prompt_tokens + (6 / 1e6) * result.usage?.completion_tokens
+          : 0;
     },
   },
 };
@@ -54,9 +123,29 @@ function skipHeaders(headers, skipList) {
   return result;
 }
 
+async function mongoRequest(action, params, { MONGODB_API_KEY, MONGODB_APP_ID }) {
+  return await fetch(`https://data.mongodb-api.com/app/${MONGODB_APP_ID}/endpoint/data/v1/action/${action}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      apiKey: MONGODB_API_KEY,
+    },
+    body: JSON.stringify({ dataSource: "iitm-tds", database: "iitm-tds-usage", collection: "user", ...params }),
+  }).then((r) => r.json());
+}
+
 function jsonResponse({ code, ...rest }) {
-  return new Response(JSON.stringify(rest), {
+  return new Response(JSON.stringify(rest, null, 2), {
     status: code,
     headers: { "content-type": "application/json" },
   });
+}
+
+class CustomError extends Error {
+  constructor({ code, message }) {
+    super(message);
+    this.code = code;
+    this.name = this.constructor.name;
+  }
 }
